ac24f5caa403825c4d8f9ba8a3cd1bcd0eaf24d0
Michael Morgan <michael1986morgan@gmail.com>
2018-05-28 08:04:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " "," ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+  puts "  |  |  /n"
+  puts "-----------/n"
+  puts "  |  |  /n"
+  puts "-----------/n"
+  puts "  |  |  /n"  
+end
