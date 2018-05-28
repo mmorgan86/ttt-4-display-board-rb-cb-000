4991fdff9ec8a8b0537d4b2a7e90083a9286f0ec
Michael Morgan <michael1986morgan@gmail.com>
2018-05-28 08:30:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = [" ", "X", " ", " ", "X", " ", " ", "X", " "]
 def display_board(board)
-  rows[0] =  "   |   |   "
-  rows[1] =  "-----------"
-  rows[2] =  "   |   |   "
-  rows[3] =  "-----------"
-  rows[4] =  "   |   |   "
+  rows[0] =  "   |   |   /n"
+  rows[1] =  "-----------/n"
+  rows[2] =  "   |   |   /n"
+  rows[3] =  "-----------/n"
+  rows[4] =  "   |   |   /n"
 end
